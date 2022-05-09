e21297be4563c9026db5ec223201ba4b260b1ac2
Diana Martinez <diananerdoficial@gmail.com>
2022-05-09 17:40:48 -0500

    Uso de state y mapState

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,11 @@
 import { createStore } from 'vuex'
 
 const store = createStore({
-  
+  state() {
+    return {
+      username: 'Diana.Nerd'
+    }
+  }
 })
 
 export default store
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@ import { RouterView, RouterLink } from 'vue-router'
 import InputSearch from '@/components/InputSearch.vue'
 import ProfileCard from '@/components/ProfileCard.vue'
 import ChatItem from '@/components/ChatItem.vue'
-import store from '@/store/store.js'
+import { mapState } from 'vuex'
 
 export default {
   components: {
@@ -15,7 +15,6 @@ export default {
   },
   data() {
     return {
-      store,
       search: '',
       profile: {
         username: 'Diana Nerd',
@@ -32,7 +31,9 @@ export default {
       ]
     }
   },
-
+  computed: {
+    ...mapState(['username'])
+  },
 }
 </script>
 
@@ -42,7 +43,7 @@ export default {
     <InputSearch v-model="search" />
     <ProfileCard
       :avatar="profile.avatar"
-      :username="store.username"
+      :username="username"
       :status="profile.status"
     />
     <RouterLink to="/" class="channels-title">Canales <Icon icon="carbon:hashtag" /></RouterLink>
