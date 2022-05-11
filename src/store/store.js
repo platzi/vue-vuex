743d8444068fd097c7b03d20bd3612f436eb91f9
Diana Martinez <diananerdoficial@gmail.com>
2022-05-11 13:37:10 -0500

    Ejemplo de normalizacion de datos en chatapp

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,49 @@
-import { reactive } from 'vue'
-
-const store = reactive({
-  username: 'diana.nerd',
-  updateUsername(username) {
-    this.username = username
-  }
-})
-
-export default store
+const store = {
+  profile: {
+    username: ''
+  },
+  contacts: [
+    { 
+      id: 1,
+      username: 'User 3'
+    },
+    {
+      id: 2,
+      username: 'User 2'
+    }
+  ],
+  messages: [
+    {
+      id: 1,
+      msg: 'Hola :D',
+      author: 1
+    },
+    {
+      id: 2,
+      msg: 'Hola :D',
+      author: 2
+    },
+    {
+      id: 3,
+      msg: 'Hola :D',
+      author: 1
+    },
+    {
+      id: 4,
+      msg: 'Hola :D',
+      author: 2
+    }
+  ],
+  channels: [
+    {
+      id: 1,
+      name: 'Canal 1',
+      messages: [1,2]
+    }, 
+    {
+      id: 2,
+      name: 'Canal 2',
+      messages: [3,4]
+    }
+  ]
+}
